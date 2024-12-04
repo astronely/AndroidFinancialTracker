6955ecfc1a0e6a5c588a428aa3da52088c31e2ad
astronely <nikborowik200394@gmail.com>
2024-12-05 00:37:22 +0500

    add: fragments for navbar

--- app/src/main/java/com/susu/bankapp/MainActivity.java
+++ app/src/main/java/com/susu/bankapp/MainActivity.java
@@ -4,16 +4,52 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
+
+    private HomeFragment homeFragment;
+    private IncomeFragment incomeFragment;
+    private ExpenseFragment expenseFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        incomeFragment = new IncomeFragment();
+        expenseFragment = new ExpenseFragment();
+        setFragment(homeFragment);
+
+        navigationView = findViewById(R.id.bottomNavigation);
+        navigationView.setSelectedItemId(R.id.nav_main);
+
+        navigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_incomes:
+                    setFragment(incomeFragment);
+                    return true;
+                case R.id.nav_expenses:
+                    setFragment(expenseFragment);
+                    return true;
+                case R.id.nav_main:
+                    setFragment(homeFragment);
+                    return true;
+                default:
+                    return false;
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
